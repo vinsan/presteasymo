1d8fe8d490c0f93bdd3ff43726f332482b155fba
Vincenzo Santoro <vincenzosantoro@10-24-48-26.0200.sal.it.iosda.org>
2017-04-03 11:11:18 +0200

    setup

---
+++ presteasymo/presteasymo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  presteasymo
+//
+//  Created by Vincenzo Santoro on 03/04/2017.
+//  Copyright © 2017 Team 2.4. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
